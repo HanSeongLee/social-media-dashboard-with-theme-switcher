5496a7d8d3d3fd115cecc65018a1b6f1d0fc280a
HanSeong Lee <jkhanseong@gmail.com>
2022-01-31 02:43:10 +0900

    ADD | React theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.scss';
+import {ThemeProvider} from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider defaultTheme={'light'}
+                     attributes={'class'}
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+  );
 }
 
 export default MyApp
