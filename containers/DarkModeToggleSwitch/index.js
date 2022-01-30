6ddbd282e49425ccb6d02e4200750dd8639e2c0b
HanSeong Lee <jkhanseong@gmail.com>
2022-01-31 04:19:12 +0900

    ADD | Dark mode toggle switch

---
+++ containers/DarkModeToggleSwitch/index.js
@@ -0,0 +1,17 @@
+import React, {useCallback} from 'react';
+import ToggleSwitch from "../../components/ToggleSwitch";
+import {useTheme} from "next-themes";
+
+const DarkModeToggleSwitch = () => {
+    const {theme, setTheme} = useTheme();
+
+    const onChange = useCallback((toggle) => {
+        setTheme(toggle ? 'dark' : 'light');
+    }, []);
+
+    return (
+        <ToggleSwitch onChange={onChange} />
+    );
+};
+
+export default DarkModeToggleSwitch;
